33e04125d6aa537f4bf66f998b8cd9e1c66d668b
Bartosz Turecki <bartosz.turecki@gmail.com>
2022-03-14 20:15:46 +0100

    Add remaining projects to solution.

---
+++ DataLibrary/Models/Abstract/IUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Models.Abstract
+{
+    public interface IUser
+    {
+        int Id { get; }
+        public string Name { get; }
+        public string Surname { get; }
+        public DateTime DateOfBirth { get; }
+        bool IsAdmin { get; }
+
+    }
+}
