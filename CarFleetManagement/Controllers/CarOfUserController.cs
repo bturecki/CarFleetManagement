13a1999e144bf36b938e4fbc2b419e8722bae180
Bartosz Turecki <bartosz.turecki@gmail.com>
2022-03-22 19:15:36 +0100

    Add list of cars of a user.

---
+++ CarFleetManagement/Controllers/CarOfUserController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarFleetManagement.Controllers
+{
+    public class CarOfUserController : Controller
+    {
+        // GET: CarOfUserController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CarOfUserController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CarOfUserController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CarOfUserController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CarOfUserController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CarOfUserController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CarOfUserController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CarOfUserController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CarFleetManagement/Models/CarOfUser.cs
@@ -0,0 +1,12 @@
+﻿namespace CarFleetManagement.Models
+{
+    public class CarOfUser
+    {
+        int Id { get; set; }
+        int UserId { get; set; }
+        int CarId { get; set; }
+        string UserFullName { get; set; }
+        string CarFullName { get; set; }
+        int CarMilage { get; set; }
+    }
+}
