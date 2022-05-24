bdf22b166fc99e706f9f6aeb41b632a260f8ad1d
Bartosz Turecki <bartosz.turecki@gmail.com>
2022-05-24 20:25:32 +0200

    Added integrations tests library.

---
+++ CarFleetManagement.IntegrationsTests/UserTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFleetManagement.IntegrationsTests
+{
+    internal class UserTests
+    {
+        [Test]
+        public void Add_User_ShouldAddUserToDatabase()
+        {
+            var _guid = new Random().Next();
+            var _startCnt = DataLibrary.BusinessLogic.PersonProcessor.LoadPeople().Count();
+            DataLibrary.BusinessLogic.PersonProcessor.CreatePerson("Test name " + _guid, "Test surname " + _guid, DateTime.Now, "Test email " + _guid, false);
+            var _endCnt = DataLibrary.BusinessLogic.PersonProcessor.LoadPeople().Count();
+            Assert.That(_endCnt - _startCnt, Is.EqualTo(1)); //added 1 row
+        }
+    }
+}
